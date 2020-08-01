f8383357f09183b112811421c86b1d0f911ae8eb
lipine37 <lupine37@gmail.com>
2020-08-02 01:20:30 +0300

    changed the nodeMCU with arduino and esp8266 and added led indicaters

--- water_level_project.ino
+++ water_level_project.ino
@@ -1,15 +1,31 @@
 #include <SoftwareSerial.h>
 
-SoftwareSerial esp8266(2, 3);
+SoftwareSerial esp8266(5, 6);
 
-#define RELAY 10
-const int trigPin = 8;
-const int echoPin = 9;
+#define RELAY 9
+const int trigPin = 3;
+const int echoPin = 4;
+const int redLedPin = 10;
+const int greenLedPin = 11;
+
 
 long duration;
-int volume;
-unsigned long initialTime = 0;
-unsigned long finalTime = 1000;
+float diameter;
+float height;
+float totalVol;
+float volume;
+unsigned long initialTime = 0, finalTime = 1000;
+int ledCount;
+bool ledState = LOW;
+
+float readnumber(void){
+  float num = 0;
+  while (num == 0){
+    while (!Serial.available());
+    num = Serial.parseInt();
+  }
+  return num;
+}
 
 void writeString(String stringData) {
   esp8266.write(" ");
@@ -22,10 +38,20 @@ void setup() {
   pinMode(RELAY, OUTPUT);
   pinMode(trigPin, OUTPUT);
   pinMode(echoPin, INPUT);
+  pinMode(redLedPin, OUTPUT);
+  pinMode(greenLedPin, OUTPUT);
   Serial.begin(9600);
   esp8266.begin(115200);
   digitalWrite(RELAY, HIGH);
+  digitalWrite(redLedPin, LOW);
+  digitalWrite(greenLedPin, LOW);
   initialTime = millis();
+  Serial.println("What is the diameter of the tank");
+  diameter = readnumber();
+  Serial.println("What is the height of the tank");
+  height = readnumber();
+  totalVol = (PI * sq(diameter / 2) * height);
+  Serial.print("The total volume of the tank is: "); Serial.println(totalVol); Serial.print("mililitres");
 
 }
 
@@ -43,23 +69,44 @@ void loop() {
   duration = pulseIn(echoPin, HIGH);
 
 //  Calculating the ml
-  if(duration != 0){
-    volume = 1696.46 -(0.034 * float(duration / 2) * (22 / 7) * sq(6));
-  }
-  Serial.print("mililitres: ");
-  Serial.println(volume);
-  delay(1000);
-  if (volume <=350) {
-    Serial.println("WATER IS EMPTY");
+  volume = totalVol -(0.034 * float(duration / 2) * PI * sq(diameter / 2));
+    delay(100);
+    if (((millis() - initialTime) > finalTime)) {
+      Serial.print("mililitres: ");
+      Serial.println(volume);
+      String vol = String(volume);
+      writeString(vol);
+      initialTime = millis();
+    }
+    if (volume <=(totalVol / 4)) {
+    // Serial.println("WATER IS EMPTY");
+    digitalWrite(redLedPin, HIGH);
+    digitalWrite(greenLedPin, LOW);
     digitalWrite(RELAY, LOW);
-  }
-  else if (volume >= 1300) {
-    Serial.println("WATER IS FULL");
-    digitalWrite(RELAY, HIGH);
-  }
-  if (((millis() - initialTime) > finalTime)) {
-    String vol = String(volume);
-    writeString(vol);
-    initialTime = millis();
-  }
+    }
+    
+    else if (volume >= (totalVol * 3 / 4)) {
+      // Serial.println("WATER IS FULL");
+      digitalWrite(greenLedPin, HIGH);
+      digitalWrite(redLedPin, LOW);
+      digitalWrite(RELAY, HIGH);
+    }
+    else {
+      if(volume >= (totalVol / 2)) {
+       digitalWrite(redLedPin, LOW);
+       digitalWrite(greenLedPin, HIGH);
+      }
+      else{
+        int Count;
+        digitalWrite(greenLedPin, LOW);
+        Count = (map(volume, (totalVol / 4), (totalVol / 2), 0, 9));
+        ledCount++;
+        if(ledCount >= Count) {
+        ledState = !ledState;
+        digitalWrite(redLedPin, ledState); 
+        ledCount = 0;
+        }  
+      }
+    }
+    
 }
